4f742885fb0066ed97a82d21342f5c9c6e2f37e7
Rakshith Padmanabha <Rakshith.Padmanabha@ibm.com>
2020-01-23 12:10:28 -0600

    Update more styles

--- src/app/nav1/nav1.component.ts
+++ src/app/nav1/nav1.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
-import { convertActionBinding } from '@angular/compiler/src/compiler_util/expression_converter';
+import {convertActionBinding} from '@angular/compiler/src/compiler_util/expression_converter';
 
 @Component({
   selector: 'app-nav1',
@@ -8,10 +8,10 @@ import { convertActionBinding } from '@angular/compiler/src/compiler_util/expres
   styleUrls: ['./nav1.component.scss']
 })
 export class Nav1Component implements OnInit {
-name: string;
-contact: string;
-country: string;
-opened = false;
+  name: string;
+  contact: string;
+  country: string;
+  opened = false;
 
   categorie_array = [
     {
@@ -28,26 +28,27 @@ opened = false;
     }
   ];
 
-  constructor() { }
+  constructor() {
+  }
 
   dropTest(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.categorie_array, event.previousIndex, event.currentIndex);
   }
 
-  add(){
-this.categorie_array.push({
-  name: this.name,
-  seq: this.categorie_array.length + 1,
+  add() {
+    this.categorie_array.push({
+      name: this.name,
+      seq: this.categorie_array.length + 1,
 
-});
-this.opened = false;
-this.name ='';
+    });
+    this.opened = false;
+    this.name = '';
   }
 
   delete(item) {
     for (let i = 0; i < this.categorie_array.length; i++) {
       if (this.categorie_array[i].name === item.name) {
-      this.categorie_array.splice(i, 1);
+        this.categorie_array.splice(i, 1);
       }
     }
   }
